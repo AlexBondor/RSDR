2182b7e2fdc8917b3e13f8fe19e8140e1695dae5
alexbondor <alexandru.viorel.bondor@gmail.com>
2016-01-10 18:42:38 +0200

    Removed unused header

--- RSDR/RSDR/Source.h
+++
@@ -1,7 +0,0 @@
-void CreateGUI();
-Mat Dilate(Mat, int amount);
-Mat Erode(Mat, int amount);
-
-void OnDilatationTrackbar(int, void*);
-void OnErosionTrackbar(int, void*);
-void OnCleanTrackbar(int, void*);
